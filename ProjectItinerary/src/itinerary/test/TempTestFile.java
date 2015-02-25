c33d4e9497bb6b7efa9da005f31fb830bd43b6ba
ianngiaw <ian@u.nus.edu>
2015-02-25 18:30:31 +0800

    Added a temp file into the package to be used for Junit tests

---
+++ ProjectItinerary/src/itinerary/test/TempTestFile.java
@@ -0,0 +1,14 @@
+package itinerary.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TempTestFile {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
