1a99dcdd01808038f76d77557011bcc582f5f84b
gableh <gable.heng@gmail.com>
2015-02-26 18:18:21 +0800

    Added JSONConverter class and test cases

---
+++ ProjectItinerary/src/itinerary/main/JSONConverter.java
@@ -0,0 +1,27 @@
+package itinerary.main;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+
+public class JSONConverter {
+	
+	public static List<String> convertTaskList(List<Task> taskList){
+		List<String> json = new ArrayList<String>();
+		Gson gson = new Gson();
+		for (Task task : taskList ){
+			json.add(gson.toJson(task));
+		}
+		return json;
+	}
+	public static List<Task> convertJsonList(List<String> jsonList){
+		List<Task> taskList = new ArrayList<Task>();
+		Gson gson = new Gson();
+		for (String json : jsonList ){
+			taskList.add(gson.fromJson(json,Task.class));
+		}
+		return taskList;
+	}
+	
+	
+
+}
