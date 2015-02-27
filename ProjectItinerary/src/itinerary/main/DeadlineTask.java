4643c956700e7af172c207f50c4e138745f482ca
ianngiaw <ian@u.nus.edu>
2015-02-27 17:17:04 +0800

    Added DeadlineTask class

---
+++ ProjectItinerary/src/itinerary/main/DeadlineTask.java
@@ -0,0 +1,22 @@
+package itinerary.main;
+
+import java.util.Calendar;
+
+public class DeadlineTask extends Task {
+	private Calendar deadline;
+
+	//@author generated
+	public DeadlineTask(int lineNumber, String text, String category,
+			boolean isPriority, boolean isComplete, Calendar deadline) {
+		super(lineNumber, text, category, isPriority, isComplete);
+		this.deadline = deadline;
+	}
+
+	public Calendar getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Calendar deadline) {
+		this.deadline = deadline;
+	}
+}
