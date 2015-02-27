7bd8e34fdc19486ead74ceda7ab413af5b2c50b4
Sean-Tay <sean@u.nus.edu>
2015-02-28 02:31:50 +0800

    Added a readJSONList() into JsonIOHandler.java to add functionality.

--- ProjectItinerary/src/itinerary/main/JsonIOHandler.java
+++ ProjectItinerary/src/itinerary/main/JsonIOHandler.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
@@ -94,7 +95,8 @@ public class JsonIOHandler {
 
     /**
      * Reads all the lines in a given File object and returns a String object
-     * containing all the JSONs, each separated by a newline operator.
+     * containing all the JSONs, each separated by a newline character. This
+     * String object can then be printed.
      * 
      * @param currFile
      *            The File object to read from.
@@ -128,5 +130,46 @@ public class JsonIOHandler {
         }
 
         return sb.toString();
+    }
+
+    /**
+     * 
+     * Reads all the lines in a given File object and returns a List<String>
+     * object containing all the JSONs in the given file.
+     * 
+     * @param currFile
+     * @return A List<String> object containing all separated JSON strings in
+     *         the given file.
+     */
+    public static List<String> readJSONList(File currFile) {
+
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        List<String> jsonList = new ArrayList<String>();
+
+        try {
+            FileInputStream reader = new FileInputStream(currFile);
+            InputStreamReader isr = new InputStreamReader(reader);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+
+            while ((line = bufferedReader.readLine()) != null) {
+
+                sb.append(line);
+                jsonList.add(sb.toString());
+                sb.setLength(0);  //To clear buffer
+            }
+
+            bufferedReader.close();
+
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        return jsonList;
     }
 }
