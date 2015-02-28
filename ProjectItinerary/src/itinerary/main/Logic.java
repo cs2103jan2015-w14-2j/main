460ec1930922c439e1a547d0ab31b9e77645fd16
ianngiaw <ian@u.nus.edu>
2015-02-28 11:38:13 +0800

    Added some structure to Logic

--- ProjectItinerary/src/itinerary/main/Logic.java
+++ ProjectItinerary/src/itinerary/main/Logic.java
@@ -1,6 +1,25 @@
 package itinerary.main;
 
+import java.util.List;
+
 //@author A0121437N
 public class Logic {
+	private Storage storage;
+	private Search search;
+	private History history;
 	
+	public Logic (String fileName) {
+		this.storage = new StorageStub(fileName);
+		this.search = new Search();
+		this.history = new History();
+	}
+	
+	public List<Task> executeUserInput (String userInput) {
+		// Parse command
+		
+		// Determine actions to be taken and take them
+		
+		// Get all tasks from storage and return to UI
+		return storage.getAllTasks();
+	}
 }
