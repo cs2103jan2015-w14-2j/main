28b521654c1f71032dc52bb212ad8d331792df6d
ianngiaw <ian@u.nus.edu>
2015-02-28 11:35:33 +0800

    Completed History, pending tests

--- ProjectItinerary/src/itinerary/main/History.java
+++ ProjectItinerary/src/itinerary/main/History.java
@@ -1,5 +1,30 @@
 package itinerary.main;
 
+//@author A0121437N
 public class History {
-
+	private HistoryNode currentPoint;
+	
+	public History() {
+		this.currentPoint = null;
+	}
+	
+	public void addNewState (State state) {
+		currentPoint = HistoryNode.createNewNode(currentPoint, state);
+	}
+	
+	public State goBack () {
+		if (currentPoint.getBack() != null) {
+			currentPoint = currentPoint.getBack();
+			return currentPoint.getCurrentState();
+		}
+		return null;
+	}
+	
+	public State goForward () {
+		if (currentPoint.getForward() != null) {
+			currentPoint = currentPoint.getForward();
+			return currentPoint.getCurrentState();
+		}
+		return null;
+	}
 }
