32eb2e0f92f0285a1e7b326d4372727d3f2e0f66
gableh <gable.heng@gmail.com>
2015-03-05 01:08:14 +0800

    JsonConverter updated to accomodate Search

--- ProjectItinerary/src/itinerary/main/JsonConverter.java
+++ ProjectItinerary/src/itinerary/main/JsonConverter.java
@@ -2,6 +2,7 @@ package itinerary.main;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import com.google.gson.Gson;
 
 //@author A0121810Y
@@ -15,21 +16,31 @@ public class JsonConverter {
         }
         return json;
     }
-
+    public static <T extends Task> List<String> getTypeList(List<T> taskList){
+    	List<String> list = new ArrayList<String>();
+    	for(T task : taskList){
+    		list.add(task.getClass().toString());
+    	}
+    	return list;
+    }
     public static <T extends Task> List<T> convertJsonList(List<String> jsonList,
-                                                           String classType) {
+                                                           List<String> typeList) {
         List<T> taskList = new ArrayList<T>();
         Gson gson = new Gson();
-        for (String json : jsonList) {
-            if (classType.equals("task")) {
-                taskList.add((T) gson.fromJson(json, Task.class));
-            }
-            if (classType.equals("scheduletask")) {
-                taskList.add((T) gson.fromJson(json, ScheduleTask.class));
-            }
-            if (classType.equals("deadlinetask")) {
-                taskList.add((T) gson.fromJson(json, DeadlineTask.class));
-            }
+
+        for (int  i=0;i<typeList.size();i++) {
+        	String[] taskString = typeList.get(i).replace(".", " ").split(" ");  
+        	String task = taskString[3];
+			if (task .equals("Task")) {
+        		taskList.add((T) gson.fromJson(jsonList.get(i), Task.class));
+        	}
+        	if (task.equals("ScheduleTask")) {
+        		taskList.add((T) gson.fromJson(jsonList.get(i), ScheduleTask.class));
+
+        	}
+        	if (task.equals("DeadlineTask")) {
+        		taskList.add((T) gson.fromJson(jsonList.get(i), DeadlineTask.class));
+        	}
 
         }
         return taskList;
