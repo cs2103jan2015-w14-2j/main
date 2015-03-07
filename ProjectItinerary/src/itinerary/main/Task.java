56abb09aeb1d2ddada854b9c4024c8209e1068ac
Sean-Tay <sean@u.nus.edu>
2015-03-07 14:10:50 +0800

    Fixed a bug in setCategory() in Task.

--- ProjectItinerary/src/itinerary/main/Task.java
+++ ProjectItinerary/src/itinerary/main/Task.java
@@ -57,7 +57,7 @@ public class Task implements Cloneable {
         
         if (category == null) {
             
-            text = "";
+            category = "";
         }
         
         this.category = category;
