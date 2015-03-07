aa67f7e6fa8872c5b2653d1502d8daf0498d4c75
ianngiaw <ian@u.nus.edu>
2015-03-07 18:38:12 +0800

    Added StorageException class to be thrown by any methods manipulating storage

---
+++ ProjectItinerary/src/itinerary/main/StorageException.java
@@ -0,0 +1,14 @@
+package itinerary.main;
+
+public class StorageException extends Exception {
+	private static final long serialVersionUID = 1L;
+	private static final String DEFAULT_MESSAGE = "An error occurred in storage!";
+	
+	public StorageException (String message) {
+		super(message);
+	}
+	
+	public StorageException () {
+		super(DEFAULT_MESSAGE);
+	}
+}
