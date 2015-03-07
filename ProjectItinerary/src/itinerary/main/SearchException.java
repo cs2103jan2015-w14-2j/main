5bbfd17f7349c5341bad434a6692b868418e0eed
gableh <gable.heng@gmail.com>
2015-03-08 01:47:17 +0800

    add SearchException

---
+++ ProjectItinerary/src/itinerary/main/SearchException.java
@@ -0,0 +1,18 @@
+package itinerary.main;
+//@author A0121810Y
+public class SearchException extends Exception {    
+    /**
+	 * 
+	 */
+    private static final long serialVersionUID = 7854129702281405376L;
+	private static final String DEFAULT_MESSAGE = "An error occurred in search!";
+	
+	public SearchException (String message) {
+		super(message);
+	}
+	
+	public SearchException () {
+		super(DEFAULT_MESSAGE);
+	
+	}
+}
