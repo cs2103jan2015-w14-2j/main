927d4909bb8a4ce0ca47e414d8b5a51c7d9e6d87
Sean-Tay <sean@u.nus.edu>
2015-03-12 22:17:37 +0800

    Edited JavaDoc in Storage.

--- ProjectItinerary/src/itinerary/main/Storage.java
+++ ProjectItinerary/src/itinerary/main/Storage.java
@@ -6,26 +6,32 @@ import java.util.List;
 public abstract class Storage {
 
     /**
-     * Call this to add a task to the file.
+     * Call this to add a task to the file. Asserts that task cannot be null.
      * 
-     * @param task the task to be added
-     * @throws StorageException thrown if any error occurs trying to add task
+     * @param task
+     *            the task to be added
+     * @throws StorageException
+     *             thrown if any error occurs trying to add task
      */
     abstract void addTask(Task task) throws StorageException;
 
     /**
-     * Call this to edit a task in the file.
+     * Call this to edit a task in the file. Asserts that task cannot be null.
      * 
-     * @param task the taskId to be edited with the details to be modified
-     * @throws StorageException thrown if any error occurs trying to edit task
+     * @param task
+     *            the taskId to be edited with the details to be modified
+     * @throws StorageException
+     *             thrown if any error occurs trying to edit task
      */
     abstract void editTask(Task task) throws StorageException;
 
     /**
-     * Call this to delete a line in the file.
+     * Call this to delete a line in the file. Asserts that task cannot be null.
      * 
-     * @param task the taskId to be deleted
-     * @throws StorageException thrown if any error occurs trying to delete task
+     * @param task
+     *            the taskId to be deleted
+     * @throws StorageException
+     *             thrown if any error occurs trying to delete task
      */
     abstract void deleteTask(Task task) throws StorageException;
 
@@ -39,23 +45,30 @@ public abstract class Storage {
     /**
      * Call this to delete everything from the file.
      * 
-     * @throws StorageException thrown if any error occurs trying to clear all tasks
+     * @throws StorageException
+     *             thrown if any error occurs trying to clear all tasks
      */
     abstract void clearAll() throws StorageException;
 
     /**
      * Call this to undo replace all the tasks in file with the tasks in tasks.
+     * Asserts that tasks cannot be null.
      * 
-     * @param tasks A List<Task> object containing all the tasks to be refilled
-     * @throws StorageException thrown if any error occurs trying to refill all tasks
+     * @param tasks
+     *            A List<Task> object containing all the tasks to be refilled
+     * @throws StorageException
+     *             thrown if any error occurs trying to refill all tasks
      */
     abstract void refillAll(List<Task> tasks) throws StorageException;
-    
+
     /**
-     * Called by editTask to overwrite originalTask's non-null variables in details
+     * Called by editTask to overwrite originalTask's non-null variables in
+     * details
      * 
-     * @param originalTask the task object to be edited
-     * @param details the details to be added or replaced
+     * @param originalTask
+     *            the task object to be edited
+     * @param details
+     *            the details to be added or replaced
      * @return a new Task object with updated details
      */
     //@author A0121437N
