849bc38c920ce74adae920335d529f7b409c2f22
ianngiaw <ian@u.nus.edu>
2015-03-17 18:30:01 +0800

    New UI package for JavaFX related files and stuff

---
+++ ProjectItinerary/src/itinerary/UI/UserInterface.java
@@ -0,0 +1,18 @@
+package itinerary.UI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application {
+
+	public static void main (String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		stage.show();
+	}
+	
+}
