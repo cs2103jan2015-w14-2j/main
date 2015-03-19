20dc68f02dcd9b27a495d05892808f928d461227
ianngiaw <ian@u.nus.edu>
2015-03-19 21:46:10 +0800

    Extracted window title as a constant

--- ProjectItinerary/src/itinerary/UI/UserInterface.java
+++ ProjectItinerary/src/itinerary/UI/UserInterface.java
@@ -9,16 +9,19 @@ import javafx.stage.Stage;
 //@author A0121437N
 public class UserInterface extends Application {
 
+	private static final String WINDOW_TITLE = "ITnerary";
+	private static final String WINDOW_FILE_NAME = "ApplicationWindow.fxml";
+
 	public static void main (String[] args) {
 		launch(args);
 	}
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("ApplicationWindow.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource(WINDOW_FILE_NAME));
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
-		stage.setTitle("ITnerary");
+		stage.setTitle(WINDOW_TITLE);
 		stage.show();
 	}
 	
