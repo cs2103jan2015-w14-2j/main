44c40653584b5098cb433b90a04e40a9aa2eaccb
ianngiaw <ian@u.nus.edu>
2015-03-29 01:31:21 +0800

    Adjusted layout

--- ProjectItinerary/src/itinerary/userinterface/FileNameRequestDialog.java
+++ ProjectItinerary/src/itinerary/userinterface/FileNameRequestDialog.java
@@ -4,14 +4,18 @@ import itinerary.storage.Storage;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class FileNameRequestDialog extends Application {
 	
+	private static final String LABEL_TEXT = "Preferred file name or path below";
+	private static final String TITLE = "Enter file name";
 	Stage stage;
 	NameRequestListener listener;
 	Label label;
@@ -24,15 +28,18 @@ public class FileNameRequestDialog extends Application {
 	@Override
 	public void start(Stage dialogStage) throws Exception {
 		this.stage = dialogStage;
-		dialogStage.setTitle("Enter file name");
+		Insets insets = new Insets(5.0);
+		dialogStage.setTitle(TITLE);
 		
-		label = new Label("Please enter your preferred file name or path below");
+		label = new Label(LABEL_TEXT);
+		label.setFont(new Font(label.getFont().getFamily(), 15));
 		
 		fileNameTextField = new TextField();
-		fileNameTextField.setPromptText("Eg. example.txt or C:\\folder\\example.txt");
 		fileNameTextField.setOnAction(enterPressed);
 		
 		VBox vbox = new VBox();
+		vbox.setSpacing(5.0);
+		vbox.setPadding(insets);
 		vbox.getChildren().addAll(label, fileNameTextField);
 		
 		Scene scene = new Scene(vbox);
