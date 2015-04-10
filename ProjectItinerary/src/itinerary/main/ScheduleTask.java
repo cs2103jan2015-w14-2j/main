8a1b069d639fdc45fa758eede6c71a177abfbf6f
ianngiaw <ian@u.nus.edu>
2015-04-10 22:09:43 +0800

    Removed commented out code

--- ProjectItinerary/src/itinerary/main/ScheduleTask.java
+++ ProjectItinerary/src/itinerary/main/ScheduleTask.java
@@ -9,8 +9,6 @@ public class ScheduleTask extends Task implements Cloneable {
     private Calendar fromDate;
     private Calendar toDate;
 
-    //@author generated
-
     public ScheduleTask(Integer taskId, String text, String category,
                         Boolean isPriority, Boolean isComplete,
                         Calendar fromDate, Calendar toDate) {
@@ -34,23 +32,6 @@ public class ScheduleTask extends Task implements Cloneable {
     public void setToDate(Calendar toDate) {
         this.toDate = toDate;
     }
-
-   /* //@author A0121437N
-    @Override
-    public void updateDetails(Task details) {
-        super.updateDetails(details);
-        if (details instanceof ScheduleTask) {
-            ScheduleTask scheduleDetails = (ScheduleTask) details;
-
-            if (scheduleDetails.getFromDate() != null) {
-                this.setFromDate(scheduleDetails.getFromDate());
-            }
-
-            if (scheduleDetails.getToDate() != null) {
-                this.setToDate(scheduleDetails.getToDate());
-            }
-        }
-    }*/
 
     //@author A0121409R
     public ScheduleTask clone() {
