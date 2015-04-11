a44e4fa3ea3b016f12d87809d114fac7e15a9335
gableh <gable.heng@gmail.com>
2015-04-11 19:44:51 +0800

    updated testcases/added comments.

--- ProjectItinerary/src/test/SearchTest.java
+++ ProjectItinerary/src/test/SearchTest.java
@@ -31,119 +31,169 @@ public class SearchTest {
 	public Calendar testFromDate;
 	public Calendar testToDate;
 	public Calendar testDeadlineDate;
-	public ScheduleTask task;
-	public ScheduleTask task2;
-	public DeadlineTask task3;
-	public ScheduleTask task4;
+	public ScheduleTask scheduleTask;
+	public ScheduleTask basicTask;
+	public ScheduleTask advancedTask;
+	public DeadlineTask priorityTask;
+	public ScheduleTask completeTask;
+	public DeadlineTask categoryTask;
+	public DeadlineTask deadlineTask;
+	public List<Task> testList;
 	public Gson gson;
 	public List<Task> jsonList;
 	public String classType;
-	
+	Calendar testToDate2;
+	Calendar testFromDate2;
+	/**
+	 * Initialization, populates a list with several tasks to be searched for later on,
+	 * each task corresponds to a specific field.
+	 */
 	@Before
 	public void init(){
+		gson = new Gson();
+		jsonList = new ArrayList<Task>();
 		
 		fromDate = Calendar.getInstance();
 		toDate = Calendar.getInstance();
 		testFromDate = Calendar.getInstance();
 		testToDate = Calendar.getInstance();
 		testDeadlineDate = Calendar.getInstance();
-		testFromDate.set(2015,3,20,0,0);
-		testToDate.set(2015,3,25,23,59);
-		testDeadlineDate.set(2015,3,25,23,59);
-		Calendar testFromDate2 = Calendar.getInstance();
-		testFromDate2.set(2016,5,28,21,59);
-		Calendar testToDate2 = Calendar.getInstance();
-		testToDate2.set(2015,3,24,23,59);
-
-		task = new ScheduleTask(1, "testtest gjhjy", "testcat",
-				 true,
-				true,fromDate, toDate);
-		task2 = new ScheduleTask(2, "test i want to eat pies", "testcat",
-				 true,
-				true,fromDate, toDate);
-		gson = new Gson();
-		jsonList = new ArrayList<Task>();
-		jsonList.add(task);
-		jsonList.add(task2);
-		task3 = new DeadlineTask(6, " true deadline hello everybody", "testcat",
-				 true,
-				true,testDeadlineDate);
-		task4 =new ScheduleTask(7, "true schedule hello everybody!", "testcat",
+		testFromDate.set(2016,3,20,0,0);
+		testToDate.set(2016,3,25,23,59);
+		testDeadlineDate.set(2016,3,24,23,59);
+		testFromDate2 = Calendar.getInstance();
+		testFromDate2.set(2016,2,28,21,59);
+		testToDate2 = Calendar.getInstance();
+		testToDate2.set(2016,4,24,23,59);
+		advancedTask = new ScheduleTask(2, "Basic Task", "Advanced Category",
 				 false,
-				false,testFromDate2, testToDate2);
-		jsonList.add(new ScheduleTask(3, "fest", "testcat",
-				 true,
-				true,fromDate, toDate));
-		jsonList.add(new ScheduleTask(4, "hello everybody!", "testcat",
+				false,Calendar.getInstance(), Calendar.getInstance());
+		deadlineTask = new DeadlineTask(0, "Test deadline", "Test Category",
 				 false,
-				false,fromDate, toDate));
-		jsonList.add(new DeadlineTask(5, "hello everybody", "testcat",
+				false,testDeadlineDate);
+		scheduleTask = new ScheduleTask(1, "Test ScheduleTask1", "Test Category",
+				 false,
+				false,testFromDate, testToDate);
+		basicTask = new ScheduleTask(2, "Basic Task", "Test Category",
+				 false,
+				false,Calendar.getInstance(), Calendar.getInstance());
+		categoryTask = new DeadlineTask(5, "Add Deadline Task", "Find This Category",
+				 false,
+				false,Calendar.getInstance());
+		priorityTask = new DeadlineTask(6, "Add Priority Task 1", "Test Category",
 				 true,
-				true,fromDate));
-		jsonList.add(task3);
-		jsonList.add(task4);
+				false,Calendar.getInstance());
+		completeTask =new ScheduleTask(7, "Add Completed Task 2", "Test Category",
+				 false,
+				true,Calendar.getInstance(), Calendar.getInstance());
+		jsonList.add(new ScheduleTask(3, "Add Schedule Task 3", "Test Category",
+				 false,
+				false,Calendar.getInstance(), Calendar.getInstance()));
+		jsonList.add(new ScheduleTask(4, "Add Schedule Task 4", "Test Category",
+				 false,
+				false,Calendar.getInstance(), Calendar.getInstance()));
+		jsonList.add(scheduleTask);
+		jsonList.add(basicTask);
+		jsonList.add(deadlineTask);
+		jsonList.add(categoryTask);
+		jsonList.add(priorityTask);
+		jsonList.add(completeTask);
+		jsonList.add(advancedTask);
 	
 	}
-	//tries to query the list with the following search terms category:testcat text:wan pies
-	@Test
-	public void testSearch() throws SearchException{
+	public void testSearchMultiple() throws SearchException{
 		SearchTask task = new SearchTask();
-		String[] field = {"text","category","isPriority"};
-		String[] catArray = {"testcat"};
-		List<String> fields = new ArrayList<String>();
-		fields.addAll(Arrays.asList(field));
-		List<String> catList = new ArrayList<String>(Arrays.asList(catArray));
-		task.setCategoryList(catList);
-		task.setText("wan pis");
-		task.setPriority(true);
+		task.setCategory("advanced category");
+		task.setText("Basic Text");
 		Search search = new Search(jsonList);
 		List<Task> testList = search.query(task);
-		assertEquals("test query",gson.toJson(task2),gson.toJson(testList.get(0)));
+		assertEquals("test query",gson.toJson(advancedTask),gson.toJson(testList.get(0)));
 
 	}
-	//tries to query the list with the following search terms isComplete:true
+	/**
+	 * Tests fuzzy search, after searching, the testlist should only be of size 1
+	 * and it should only contain basicTask
+	 * @throws SearchException
+	 */
 	@Test
-	public void testBasicSearch() throws SearchException{
+	public void testSearchCategory() throws SearchException{
+		SearchTask task = new SearchTask();
+		task.setCategory("Find This Category");
 		Search search = new Search(jsonList);
-		List<Task> testList = search.query("wan pis","text");
-		assertEquals("test query",gson.toJson(task2),gson.toJson(testList.get(0)));
+		List<Task> testList = search.query(task);
+		assertEquals("test query",gson.toJson(categoryTask),gson.toJson(testList.get(0)));
 
 	}
-	//tries to query the list with the following search terms isComplete:true
+	/**
+	 * Tests fuzzy search, after searching, the testlist should only be of size 1
+	 * and it should only contain basicTask
+	 * @throws SearchException
+	 */
+	@Test
+	public void testFuzzySearch() throws SearchException{
+		Search search = new Search(jsonList);
+		testList = search.query("beaic TAFF","text");
+		assertEquals("test query",gson.toJson(basicTask),gson.toJson(testList.get(0)));
+
+
+	}
+	/**
+	 * Tests wildcard search, after searching, the testlist should only be of size 1
+	 * and it should only contain basicTask
+	 * @throws SearchException
+	 */
+	@Test
+	public void testWildCardSearch() throws SearchException{
+		Search search = new Search(jsonList);
+		List<Task> testList = search.query("ba ta","text");
+		assertEquals("test query",gson.toJson(basicTask),gson.toJson(testList.get(0)));
+
+
+	}
+	/**
+	 * Tests search by isComplete, after searching, the testlist should only be of size 1
+	 * and it should only contain completeTask
+	 * @throws SearchException
+	 */
 	@Test
 	public void testisCompleteSearch() throws SearchException{
 		SearchTask task = new SearchTask();
-		String[] catArray = {"testcat"};
-		List<String> catList = new ArrayList<String>(Arrays.asList(catArray));
-		task.setCategoryList(catList);
-		task.setText("wan pis");
-		task.setPriority(true);
 		task.setComplete(true);
 		Search search = new Search(jsonList);
 		List<Task> testList = search.query(task);
-		assertEquals("test query",gson.toJson(task2),gson.toJson(testList.get(0)));
+		assertEquals("test query",gson.toJson(completeTask),gson.toJson(testList.get(0)));
 
 	}
+	/**
+	 * Tests search by priority, after searching, the testlist should only be of size 1
+	 * and it should only contain priorityTask
+	 * @throws SearchException
+	 */
 	@Test
-	public void testSearchWithDeadline() throws SearchException{
+	public void testPrioritySearch() throws SearchException{
 		SearchTask task = new SearchTask();
-		task.setText("Hello Everybody");
 		task.setPriority(true);
-		task.setDeadline(testDeadlineDate);
 		Search search = new Search(jsonList);
 		List<Task> testList = search.query(task);
-		assertEquals("test deadlineQuery",gson.toJson(task3),gson.toJson(testList.get(0)));
+		assertEquals("test query",gson.toJson(priorityTask),gson.toJson(testList.get(0)));
 
 	}
+	/**
+	 * Should return deadlineTask as well as scheduleTask.
+	 * They must be in the same order because otherwise the conversion
+	 * from json back to Task will fail.
+	 * @throws SearchException
+	 */
 	@Test
-	public void testSearchWithSchedule() throws SearchException{
+	public void testSearchDate() throws SearchException{
 		SearchTask task = new SearchTask();
-		task.setToDate(testToDate);
-		task.setComplete(true);
-		task.setFromDate(testFromDate);
+		task.setToDate(testToDate2);
+		task.setFromDate(testFromDate2);
 		Search search = new Search(jsonList);
 		List<Task> testList = search.query(task);
-		assertEquals("test scheduleQuery",gson.toJson(task4),gson.toJson(testList.get(1)));
+		assertEquals("test deadlineQuery",gson.toJson(deadlineTask),gson.toJson(testList.get(1)));
+		assertEquals("test scheduleQuery",gson.toJson(scheduleTask),gson.toJson(testList.get(0)));
 
 	}
+
 }
