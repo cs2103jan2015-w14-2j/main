212b968968670b4337b10685df40e5f647c144e1
gableh <gable.heng@gmail.com>
2015-04-11 18:36:24 +0800

    add comments

--- ProjectItinerary/src/itinerary/search/Search.java
+++ ProjectItinerary/src/itinerary/search/Search.java
@@ -42,13 +42,6 @@ import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
-
-
-
-
-
-
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -56,13 +49,9 @@ import com.google.gson.JsonParser;
 
 import java.io.IOException;
 
-/**
- * 
- * 
- *
- */
-// @author A0121810Y
+//@author A0121810Y
 public class Search {
+	private static final int NUM_HITS = 10;
 	private static final int JSON_SECOND = 24;
 	private static final int JSON_MINUTE = 20;
 	private static final String WILDCARD = "*";
@@ -104,7 +93,16 @@ public class Search {
 	IndexWriter writer;
 	Document doc;
 	Gson gson;
-	
+
+	/**
+	 * Search constructor that indexes the current tasklist using a
+	 * StandardAnalyzer and the default IndexWriter config.The tasklist must
+	 * first be converted to Json. Does not use the default STOP_WORDS_SET
+	 * provided by Lucene.
+	 * 
+	 * @param taskList
+	 * @throws SearchException
+	 */
 	public <T extends Task> Search(List<T> taskList) throws SearchException {
 		list = JsonConverter.convertTaskList(taskList);
 		parser = new JsonParser();
@@ -118,6 +116,14 @@ public class Search {
 		gson = new Gson();
 	}
 
+	/**
+	 * Creates a query out of a given SearchTask then searches it. Does this by
+	 * building a boolean query out of the not-null fields within SearchTask
+	 * 
+	 * @param task
+	 * @return
+	 * @throws SearchException
+	 */
 	public <T extends Task> List<T> query(SearchTask task)
 	        throws SearchException {
 		assert task != null;
@@ -135,7 +141,7 @@ public class Search {
 			BooleanQuery bQuery = createTextQuery(task);
 			addMustOccur(q, bQuery);
 		}
-		
+
 		if (task.getCategory() != null) {
 			BooleanQuery bQuery = createCategoryQuery(task);
 			addMustOccur(q, bQuery);
@@ -161,11 +167,25 @@ public class Search {
 		return JsonConverter.convertJsonList(hitList, hitTypeList);
 	}
 
+	/**
+	 * creates a query that searches through the category stated in the given
+	 * SearchTask
+	 * 
+	 * @param task
+	 * @return
+	 */
 	private BooleanQuery createCategoryQuery(SearchTask task) {
-	    BooleanQuery bQuery = createQuery(FIELD_CATEGORY, task.getCategory());
-	    return bQuery;
-    }
+		BooleanQuery bQuery = createQuery(FIELD_CATEGORY, task.getCategory());
+		return bQuery;
+	}
 
+	/**
+	 * Searches a boolean query, all matches are added to a hitList
+	 * 
+	 * @param q
+	 * @throws SearchException
+	 * @throws IOException
+	 */
 	private void search(BooleanQuery q) throws SearchException, IOException {
 		ScoreDoc[] hits = searchQuery(q, searcher);
 		addToHitList(hitList, searcher, hits);
@@ -178,7 +198,8 @@ public class Search {
 	}
 
 	private BooleanQuery createTextQuery(SearchTask task) {
-		BooleanQuery bQuery = createQuery(FIELD_TEXT, task.getText());
+		BooleanQuery bQuery = createQuery(FIELD_TEXT,
+		        parseString(task.getText()));
 		return bQuery;
 	}
 
@@ -204,14 +225,22 @@ public class Search {
 		return cpQuery;
 	}
 
+	/**
+	 * Used for basic search, builds a query that only searches through the text
+	 * field.
+	 * 
+	 * @param query
+	 * @param field
+	 * @return
+	 * @throws SearchException
+	 */
 	public <T extends Task> List<T> query(String query, String field)
 	        throws SearchException {
-		// The same analyzer should be used for indexing and searching
 		logger.log(Level.INFO, DISPLAY_SEARCH + query);
 		try {
 
 			BooleanQuery q = new BooleanQuery();
-			q = createQuery(field, query.toLowerCase());
+			q = createQuery(field, parseString(query));
 			search(q);
 			reader.close();
 		} catch (IOException e) {
@@ -221,6 +250,27 @@ public class Search {
 		return JsonConverter.convertJsonList(hitList, hitTypeList);
 	}
 
+	/**
+	 * All search queries are parsed first to remove any special characters as
+	 * StandardAnalyzer doesnt index them properly
+	 * 
+	 * @param query
+	 * @return
+	 */
+	private String parseString(String query) {
+		query = query.toLowerCase().replaceAll("&", " and ");
+		return query.toLowerCase().replaceAll("[^a-zA-Z0-9]", " ");
+	}
+
+	/**
+	 * adds the search results to a hitlist which will then be converted back to
+	 * a List<Task>
+	 * 
+	 * @param hitList
+	 * @param searcher
+	 * @param hits
+	 * @throws IOException
+	 */
 	private void addToHitList(ArrayList<String> hitList,
 	        IndexSearcher searcher, ScoreDoc[] hits) throws IOException {
 		hitTypeList = new ArrayList<String>();
@@ -237,9 +287,19 @@ public class Search {
 		hitTypeList.add(typeList.get(docId));
 	}
 
+	/**
+	 * Helper method for search(),searches the index for the top 10 closest
+	 * matches The number of matches to search for can be changed by changing
+	 * the numHits
+	 * 
+	 * @param q
+	 * @param searcher
+	 * @return
+	 * @throws SearchException
+	 */
 	private ScoreDoc[] searchQuery(BooleanQuery q, IndexSearcher searcher)
 	        throws SearchException {
-		int numHits = 10;
+		int numHits = NUM_HITS;
 		TopScoreDocCollector collector = TopScoreDocCollector.create(numHits);
 		try {
 			searcher.search(q, collector);
@@ -251,6 +311,14 @@ public class Search {
 		return hits;
 	}
 
+	/**
+	 * combines a fuzzyQuery and a WildcardQuery into a BooleanQuery This is for
+	 * near-match as well as wildcard search to work at the same time
+	 * 
+	 * @param field
+	 * @param Query
+	 * @return
+	 */
 	private BooleanQuery createQuery(String field, String Query) {
 		String[] splitQuery = Query.split(QUERY_DELIMITER);
 		BooleanQuery bQuery = new BooleanQuery();
@@ -261,6 +329,17 @@ public class Search {
 		return bQuery;
 	}
 
+	/**
+	 * The WILDCARD constant is placed after splitQuery[i] so that it searches
+	 * for any strings that begin with splitQuery[i], to have it search for any
+	 * string that end with splitQuery[i], place the WILDCARD constant before
+	 * it. Note that the time to search for wildCardQuerys can increase quite
+	 * dramatically the more the indexed items.
+	 * 
+	 * @param field
+	 * @param splitQuery
+	 * @return
+	 */
 	private Query getWildCardQuery(String field, String[] splitQuery) {
 		SpanQuery[] queryParts = new SpanQuery[splitQuery.length];
 		for (int i = 0; i < splitQuery.length; i++) {
@@ -273,16 +352,28 @@ public class Search {
 		return q;
 	}
 
+	/**
+	 * FuzzyQuerys are used in near match search, it done by calculating the
+	 * levenshtein distance.It is considered a hit if the text it is searching
+	 * has a levenshtein distance of LEVENSHTEIN_DISTANCE from the current
+	 * search query.
+	 * 
+	 * @param field
+	 * @param splitQuery
+	 * @return
+	 */
 	private Query getFuzzyQuery(String field, String[] splitQuery) {
 		SpanQuery[] queryParts = new SpanQuery[splitQuery.length];
 		FuzzyQuery fuzzyQuery;
 		for (int i = 0; i < splitQuery.length; i++) {
-			if(notMinLength(splitQuery, i)){
-				fuzzyQuery = createFuzzyQuery(field, splitQuery, i,LEVENSHTEIN_DISTANCE);
+			if (notMinLength(splitQuery, i)) {
+				fuzzyQuery = createFuzzyQuery(field, splitQuery, i,
+				        LEVENSHTEIN_DISTANCE);
 			} else {
-				fuzzyQuery = createFuzzyQuery(field, splitQuery, i,LEVENSHTEIN_0);
+				fuzzyQuery = createFuzzyQuery(field, splitQuery, i,
+				        LEVENSHTEIN_0);
 			}
-			
+
 			queryParts[i] = new SpanMultiTermQueryWrapper<FuzzyQuery>(
 			        fuzzyQuery);
 		}
@@ -291,15 +382,15 @@ public class Search {
 	}
 
 	private boolean notMinLength(String[] splitQuery, int i) {
-	    return splitQuery[i].length() >MIN_LENGTH;
-    }
+		return splitQuery[i].length() > MIN_LENGTH;
+	}
 
-	private FuzzyQuery createFuzzyQuery(String field, String[] splitQuery, int i,int lDistance) {
-	    FuzzyQuery fuzzyQuery;
-	    fuzzyQuery = new FuzzyQuery(new Term(field,
-	        splitQuery[i]),lDistance);
-	    return fuzzyQuery;
-    }
+	private FuzzyQuery createFuzzyQuery(String field, String[] splitQuery,
+	        int i, int lDistance) {
+		FuzzyQuery fuzzyQuery;
+		fuzzyQuery = new FuzzyQuery(new Term(field, splitQuery[i]), lDistance);
+		return fuzzyQuery;
+	}
 
 	public TermQuery createCompleteOrPriorityQuery(boolean setTrue, String field)
 	        throws SearchException {
@@ -315,29 +406,41 @@ public class Search {
 	public BooleanQuery createDeadlineQuery(Calendar deadline)
 	        throws SearchException {
 		BooleanQuery bQuery = new BooleanQuery();
-		String sDeadline =DateTools.dateToString(deadline.getTime(), Resolution.SECOND);
+		String sDeadline = DateTools.dateToString(deadline.getTime(),
+		        Resolution.SECOND);
 		TermRangeQuery rangeDeadlineQ = TermRangeQuery.newStringRange(
-		        FIELD_DEADLINE, sDeadline, sDeadline,
-		        true, true);
+		        FIELD_DEADLINE, sDeadline, sDeadline, true, true);
 		bQuery.add(rangeDeadlineQ, BooleanClause.Occur.MUST);
 		return bQuery;
 	}
 
+	/**
+	 * Since Lucene can only search a single field at a time, this creates a
+	 * dateQuery by first searching for tasks with dates in the toDate field
+	 * that fall within the specified fromDate and toDate dates inclusive and
+	 * then excluding all of those tasks that have fromDate dates from the epoch
+	 * to the specified fromDate exclusive.
+	 * 
+	 * @param toDate
+	 * @param fromDate
+	 * @return
+	 * @throws SearchException
+	 */
 	public BooleanQuery createDateQuery(Calendar toDate, Calendar fromDate)
 	        throws SearchException {
-		String sFromDate = DateTools.dateToString(fromDate.getTime(), Resolution.SECOND);
-		String sToDate = DateTools.dateToString(toDate.getTime(), Resolution.SECOND);
+		String sFromDate = DateTools.dateToString(fromDate.getTime(),
+		        Resolution.SECOND);
+		String sToDate = DateTools.dateToString(toDate.getTime(),
+		        Resolution.SECOND);
 		BooleanQuery bQuery = new BooleanQuery();
-		String epochDate = DateTools.dateToString(getEpoch().getTime(), Resolution.SECOND);
+		String epochDate = DateTools.dateToString(getEpoch().getTime(),
+		        Resolution.SECOND);
 		TermRangeQuery rangeDeadlineQ = TermRangeQuery.newStringRange(
-		        FIELD_DEADLINE, sFromDate, sToDate,
-		        true, true);
+		        FIELD_DEADLINE, sFromDate, sToDate, true, true);
 		TermRangeQuery rangeScheduleQ = TermRangeQuery.newStringRange(
-		        FIELD_TODATE, sFromDate, sToDate, true,
-		        true);
+		        FIELD_TODATE, sFromDate, sToDate, true, true);
 		TermRangeQuery rangeFromScheduleQ = TermRangeQuery.newStringRange(
-		        FIELD_FROMDATE, epochDate, sFromDate,
-		        false, false);
+		        FIELD_FROMDATE, epochDate, sFromDate, false, false);
 		bQuery.add(rangeDeadlineQ, BooleanClause.Occur.SHOULD);
 		bQuery.add(rangeScheduleQ, BooleanClause.Occur.SHOULD);
 		bQuery.add(rangeFromScheduleQ, BooleanClause.Occur.MUST_NOT);
@@ -364,30 +467,62 @@ public class Search {
 		}
 	}
 
+	/**
+	 * The fields text,category are indexed by TextField in order to tokenize
+	 * them. This is to prevent any potential errors arising from using fuzzy
+	 * and wildcard queries. Dates are indexed according to Lucene's dateTools,
+	 * StringField is used so as to preserve the string literal Priority and
+	 * isComplete is also index with StringField in order to preserve the string
+	 * literal It also stops working properly if TextField is used.
+	 * 
+	 * @param writer
+	 * @param obj
+	 * @throws IOException
+	 */
 	private void addDoc(IndexWriter writer, JsonObject obj) throws IOException {
 		doc = new Document();
 		for (Entry<String, JsonElement> entry : obj.entrySet()) {
 			String key = entry.getKey();
 			JsonElement value = entry.getValue();
 			if (key.equals(FIELD_TEXT) || key.equals(FIELD_CATEGORY)) {
-				doc.add(new TextField(key, value.toString(), Field.Store.YES));
-			} else if (key.equals(FIELD_TODATE) || key.equals(FIELD_FROMDATE) || key.equals(FIELD_DEADLINE)){
+				doc.add(new TextField(key, parseString(value.toString()),
+				        Field.Store.YES));
+			} else if (key.equals(FIELD_TODATE) || key.equals(FIELD_FROMDATE)
+			        || key.equals(FIELD_DEADLINE)) {
 				Calendar date = convertJsonToDate(value.toString());
-				doc.add(new StringField(key, DateTools.dateToString(date.getTime(), Resolution.SECOND), Field.Store.YES));
-			} else {
+				doc.add(new StringField(key, DateTools.dateToString(
+				        date.getTime(), Resolution.SECOND), Field.Store.YES));
+			} else if (key.equals(FIELD_ISCOMPLETE)
+			        || key.equals(FIELD_ISPRIORITY)) {
 				doc.add(new StringField(key, value.toString(), Field.Store.YES));
 			}
 		}
 		doc.add(new StringField(FIELD_JSON, obj.toString(), Field.Store.YES));
 		writer.addDocument(doc);
 	}
-	private Calendar convertJsonToDate(String obj){
+
+	/**
+	 * Since DateTools can only be used to convert Date objects,this function
+	 * manually converts the JSON Calendar string back into a Calendar object,
+	 * which is later converted into a Date object with Calendar.getTime().This
+	 * is done because gson is unable to convert it automatically with the
+	 * fromJson method.
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	private Calendar convertJsonToDate(String obj) {
 		Calendar date = Calendar.getInstance();
 		String[] oString = obj.split(JSON_DELIMITER);
-		date.set(Integer.parseInt(oString[JSON_YEAR]),Integer.parseInt(oString[JSON_MONTH]),Integer.parseInt(oString[JSON_DATE])
-				,Integer.parseInt(oString[JSON_HOUROFDAY]),Integer.parseInt(oString[JSON_MINUTE]),Integer.parseInt(oString[JSON_SECOND]));
+		date.set(Integer.parseInt(oString[JSON_YEAR]),
+		        Integer.parseInt(oString[JSON_MONTH]),
+		        Integer.parseInt(oString[JSON_DATE]),
+		        Integer.parseInt(oString[JSON_HOUROFDAY]),
+		        Integer.parseInt(oString[JSON_MINUTE]),
+		        Integer.parseInt(oString[JSON_SECOND]));
 		return date;
 	}
+
 	private void closeWriter(IndexWriter writer) throws IOException {
 		writer.close();
 	}
@@ -407,6 +542,5 @@ public class Search {
 		assert (fromDate.getTime().toString().equals(date.toString()));
 		return fromDate;
 	}
-
 
 }
