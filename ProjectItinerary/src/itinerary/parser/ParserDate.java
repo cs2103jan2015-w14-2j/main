33eda86eda1092daab2aec09d084bbb39fb61c12
hemolin <hemolin123@hotmail.com>
2015-04-13 02:35:12 +0800

    corrected indentation of else if

--- ProjectItinerary/src/itinerary/parser/ParserDate.java
+++ ProjectItinerary/src/itinerary/parser/ParserDate.java
@@ -217,8 +217,7 @@ public class ParserDate {
 	private static boolean isLeapYear(int year){
 		if (year % 400 == 0){ 
 			return true;
-		}
-		else if (year % 4 == 0 && year % 100 != 0){ 
+		}else if (year % 4 == 0 && year % 100 != 0){ 
 			return true;
 		}
 		return false;
